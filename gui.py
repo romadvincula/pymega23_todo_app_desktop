ad813a1d009bb67a37c816cf7d569eeee783f7f3
romadv <advi0001@flinders.edu.au>
2023-03-10 23:44:21 +1100

    Implement edit button

--- gui.py
+++ gui.py
@@ -4,11 +4,15 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter to-do', key='todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='current_todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
                    layout=[[label],
-                           [input_box, add_button]],
-                   font=('Helvetica', 20))
+                           [input_box, add_button],
+                           [list_box, edit_button]],
+                   font=('Helvetica',+ 20))
 
 while True:
     event, values = window.read()
@@ -19,6 +23,21 @@ while True:
         new_todo = values['todo'] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
+        window['current_todos'].update(values=todos)
+
+    elif event == "current_todos":
+       window['todo'].update(value=values['current_todos'][0])
+
+    elif event == "Edit":
+        todo_to_edit = values['current_todos'][0]
+        new_todo = values['todo'] + '\n'
+
+        todos = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo
+
+        functions.write_todos(todos)
+        window['current_todos'].update(values=todos)
 
     elif event == sg.WIN_CLOSED:
         break
