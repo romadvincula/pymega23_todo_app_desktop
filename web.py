f9f1fb1b1a05cd282e4aa0b2db7e45718fe5de99
romadv <advi0001@flinders.edu.au>
2023-03-12 17:34:56 +1100

    add complete to do

--- web.py
+++ web.py
@@ -16,8 +16,15 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        print(checkbox)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st._rerun()
+
 
 st.text_input(label="Add a new todo...", label_visibility="hidden", placeholder="Add a new todo...",
               on_change=add_todo, key="new_todo")
